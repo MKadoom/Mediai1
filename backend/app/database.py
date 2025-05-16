63d53ca4e5c5d2ae1dd057d910bbbe06a2a7a3fc
MKadoom <47943462+MKadoom@users.noreply.github.com>
2025-05-16 11:57:23 +0300

    Update database.py

--- backend/app/database.py
+++ backend/app/database.py
@@ -2,11 +2,10 @@ from sqlalchemy import create_engine, Column, Integer, String, Text
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./diagnosis.db"
+# Replace with your Supabase credentials
+DB_URL = "postgresql://postgres:[YOUR-PASSWORD]@db.lfykzdnftdarelufqauy.supabase.co:5432/postgres"
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
+engine = create_engine(DB_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
